b6434fe0a4733d8ae8deefaf39d6f29e704ad538
Wilson Diniz Silva <diniztte@gmail.com>
2022-01-01 19:05:32 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,6 +29,13 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-	}
+		
+		System.out.println("\n===== TEST 3: seller findAll =====");
+		
+		list = sellerDao.findAll();
 
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+	}
 }
